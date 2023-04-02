3e0ed555dbbc46399575de79f64afd663b9bd944
3x1t-5tyl3 <61988002+3x1t-5tyl3@users.noreply.github.com>
2023-04-02 10:38:16 +0200

    Further cleanup and added a logo which might or might not work. I just don't care enough tbh..

--- src/main/java/ch/exitian/largeorenodesremove/LargeOreNodesRemove.java
+++ src/main/java/ch/exitian/largeorenodesremove/LargeOreNodesRemove.java
@@ -1,23 +1,17 @@
 package ch.exitian.largeorenodesremove;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(LargeOreNodesRemove.MODID)
 public class LargeOreNodesRemove
 {
     public static final String MODID = "largeorenodesremove";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public LargeOreNodesRemove()
     {
@@ -26,7 +20,6 @@ public class LargeOreNodesRemove
         modEventBus.addListener(this::commonSetup);
 
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -35,21 +28,5 @@ public class LargeOreNodesRemove
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
 
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
-    }
 }
